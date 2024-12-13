13a361993cb1404e1450258f30099c161159cd63
GalMichaeli <michaeli.gal@gmail.com>
2024-12-13 13:09:11 +0200

    fix comma

--- src/main/kotlin/il/ac/technion/cs/sd/books/external/LineStorageFactory.kt
+++ src/main/kotlin/il/ac/technion/cs/sd/books/external/LineStorageFactory.kt
@@ -3,7 +3,7 @@ package il.ac.technion.cs.sd.books.external
 interface LineStorageFactory {
 
     /**
-     * Return the LineStorage instance associated with `fileName` if it exists.
+     * Return the LineStorage instance associated with `fileName`, if it exists.
      * Otherwise creates a new instance of LineStorage, associates it with
      * `fileName` and returns it.
      */
