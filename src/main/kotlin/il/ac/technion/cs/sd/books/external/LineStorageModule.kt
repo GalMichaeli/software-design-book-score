26dad41845ad8fcaf98f38e501229724a7d0c369
GalMichaeli <michaeli.gal@gmail.com>
2024-12-15 14:15:42 +0200

    removed singleton from factory binding

--- src/main/kotlin/il/ac/technion/cs/sd/books/external/LineStorageModule.kt
+++ src/main/kotlin/il/ac/technion/cs/sd/books/external/LineStorageModule.kt
@@ -1,12 +1,11 @@
 package il.ac.technion.cs.sd.books.external
 
 import dev.misfitlabs.kotlinguice4.KotlinModule
-import com.google.inject.Singleton
 
 import il.ac.technion.cs.sd.books.external.LineStorageFactory
 
 class LineStorageModule : KotlinModule() {
     override fun configure() {
-        bind<LineStorageFactory>().to<LineStorageFactoryImpl>().`in`<Singleton>()
+        bind<LineStorageFactory>().to<LineStorageFactoryImpl>()
     }
 }
